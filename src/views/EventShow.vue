bff8eaa5d5c1b16ea168ca348927cd27c2a271b3
vijayvardhan94 <tadimetv@oregonstate.edu>
2023-08-06 18:14:13 -0400

    adding another call to event service

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
     <h1>Show events</h1>
-    <p>Welcome to #{{ id }}</p>
+    <p>Welcome to #{{ event.title }}</p>
   </div>
 </template>
 
 <script>
+import EventService from "@/services/EventService";
 export default {
   props: ["id"],
+  data() {
+    return {
+      event: {},
+    };
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then((Response) => {
+        this.event = Response.data;
+      })
+      .catch((error) => {
+        console.log("Error:", error.Response);
+      });
+  },
 };
 </script>
