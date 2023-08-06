0847fdb62e57bb74899dceed633fbe371bd49718
vijayvardhan94 <tadimetv@oregonstate.edu>
2023-08-06 13:02:43 -0400

    base components

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -1,11 +1,47 @@
 <template>
-  <div>
-    <h1>Icon</h1>
-  </div>
+  <div class="icon-wrapper" v-html="svg"></div>
 </template>
 
 <script>
-export default {};
+import feather from "feather-icons";
+export default {
+  props: {
+    name: String,
+    width: {
+      type: [Number, String],
+      default: 24,
+    },
+    height: {
+      type: [Number, String],
+      default: 24,
+    },
+  },
+  computed: {
+    svg() {
+      return feather.icons[this.name].toSVG({
+        class: "icon",
+        width: this.width,
+        height: this.height,
+      });
+    },
+  },
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+  margin-right: 6px;
+}
+.icon {
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+}
+</style>
