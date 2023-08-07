3d5e4957314af84a897c98f4d41fcbcc69b57614
vijayvardhan94 <tadimetv@oregonstate.edu>
2023-08-07 19:45:00 -0400

    adding more computed values

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <h1>Create event, {{ $store.state.user.name }}</h1>
+    <h1>{{ userName }}</h1>
+    <p>This is the {{ userId }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    userName() {
+      return this.$store.state.user.name;
+    },
+    userId() {
+      return this.$store.state.user.id;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
